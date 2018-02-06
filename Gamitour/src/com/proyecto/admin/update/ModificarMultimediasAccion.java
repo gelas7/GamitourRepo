7d8dc1baa8bc51736f0a35777ed321873e6dd28e
Alfonso Del Busto <adelbustom@yahoo.com>
2018-02-06 10:04:01 +0100

    Modificar Multimedias

--- Gamitour/src/com/proyecto/admin/update/ModificarMultimediasAccion.java
+++ Gamitour/src/com/proyecto/admin/update/ModificarMultimediasAccion.java
@@ -1,54 +1,82 @@
 package com.proyecto.admin.update;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.proyecto.modelo.Cliente;
-import com.proyecto.modelo.Comentario;
+
 import com.proyecto.modelo.Multimedia;
-import com.proyecto.modelo.Voto;
+
+import com.proyecto.service.ServiceClientes;
+import com.proyecto.service.ServiceClientesImp;
 import com.proyecto.service.ServiceMultimedias;
 import com.proyecto.service.ServiceMultimediasImp;
+
 import com.proyecto.util.Accion;
 
-
 public class ModificarMultimediasAccion extends Accion {
-	
-	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd"); 
+
+	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
 
 	@Override
 	public String ejecutar(HttpServletRequest request, HttpServletResponse response) {
-		String idmultimedia = request.getParameter("id");
+
+		String id = request.getParameter("id");
 		String cliente = request.getParameter("cliente");
 		String fecha = request.getParameter("fecha");
 		String comentario = request.getParameter("comentario");
 		String imagen = request.getParameter("imagen");
 		String video = request.getParameter("video");
-		String pruebaDeportivaIdpruebadeportiva = request.getParameter("pruebaDeportivaIdpruebadeportiva");
-		String puntosacumulados = request.getParameter("puntosacumulados");
-		
+		String idPd = request.getParameter("pruebaD");
+		String puntos = request.getParameter("puntos");
+
+		Date date1 = null;
+
+		try {
+
+			if (fecha != "")
+				date1 = formatter.parse(fecha);
+
+		} catch (ParseException e) {
+			System.out.println("Fallo al convertir fechas. " + e.getMessage());
+		}
+
 		ServiceMultimedias sm = new ServiceMultimediasImp();
-		
+		ServiceClientes sc = new ServiceClientesImp();
+
 		Multimedia m = sm.buscarPorClave(Integer.parseInt(id));
-		
-		m.setCliente(cliente);
-		m.setFecha(fecha);
-		m.setComentario(comentario);
-		m.setImagen(imagen);
-		m.setVideo(video);
-		m.setPruebaDeportivaIdpruebadeportiva(pruebaDeportivaIdpruebadeportiva);
-		m.setPuntosacumulados(puntosacumulados);
-		m.setComentarios(comentarios);
-		m.setVideo(video);
-		
+
+		if (cliente != "") {
+			Cliente c = sc.buscarPorClave(Integer.parseInt(cliente));
+			m.setCliente(c);
+		}
+
+		if (idPd != "") {
+
+			m.setPruebaDeportivaIdpruebadeportiva(Integer.parseInt(idPd));
+		}
+		if (fecha != "")
+			m.setFecha(date1);
+
+		if (imagen != "")
+			m.setImagen(imagen);
+
+		if (video != "")
+			m.setVideo(video);
+
+		if (comentario != "")
+			m.setComentario(comentario);
+
+		if (puntos != "")
+			m.setPuntosacumulados(Integer.parseInt(puntos));
+
 		sm.actualizar(m);
-		
-		request.setAttribute("listaMultimedias", sm.buscarTodos());
-		return "mostrarMultimedias.jsp";
+
+		return "MostrarAdmin.do?div=premios";
 	}
 
 }
