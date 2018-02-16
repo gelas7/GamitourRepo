e02ea209539e2227c9085856e8c6a3697538b0d8
Alfonso Del Busto <adelbustom@yahoo.com>
2018-02-16 01:53:49 +0100

    Busqueda de comentarios en multimedia
    Falta el jsp y el servlet

--- Gamitour/src/com/proyecto/dao/MultimediasDAO.java
+++ Gamitour/src/com/proyecto/dao/MultimediasDAO.java
@@ -20,5 +20,7 @@ public interface MultimediasDAO extends GenericDAO<Multimedia, Integer> {
 	public List<Multimedia> buscarTodos();
 	
 	public List<Multimedia> mostrarMultimediasPorPd(String idprueba);
+	
+	public List<Multimedia> mostrarMultimediasPorComent(String idcomentario);
 
 }
