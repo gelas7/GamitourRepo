d1a2ba90b3158869312229db431a37cf293f9ab0
Miguel Angel <miguelaar96@github.es>
2018-02-28 10:37:30 +0100

    Sesion y arreglo de null

--- Gamitour/src/com/proyecto/admin/search/BuscarAdminAccion.java
+++ Gamitour/src/com/proyecto/admin/search/BuscarAdminAccion.java
@@ -18,8 +18,8 @@ public class BuscarAdminAccion extends Accion {
 		String email = (String) request.getParameter("email"); // Recojo los parametros del login
 		String pass = (String) request.getParameter("password");
 		Cliente c = sc.buscarClientePorEmail(email);
-		
-		System.out.println("URL: "+url);
+
+		System.out.println("URL: " + url);
 		System.out.println(email);
 		System.out.println(pass);
 		System.out.println(c);
@@ -34,7 +34,8 @@ public class BuscarAdminAccion extends Accion {
 				if (rol.equals("administrador")) {
 					System.out.println("Bienvenido admin");
 					request.getSession().setAttribute("email", c.getEmail());
-					rutaSalida = url; // MODIFICO SALIDA
+					if (url != null)
+						rutaSalida = url; // MODIFICO SALIDA
 				} else {
 					rutaSalida = "errores/error.jsp"; // MODIFICO SALIDA
 				}
